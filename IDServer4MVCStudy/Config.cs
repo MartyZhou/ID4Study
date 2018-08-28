f488b1f71328e1455838bd49d96909a667387e49
martyzhou <martyzhou@objectivasoftware.com>
2018-08-28 10:09:18 +0800

    format

--- IDServer4MVCStudy/Config.cs
+++ IDServer4MVCStudy/Config.cs
@@ -10,80 +10,83 @@ namespace IDServer4MVCStudy
         public static IEnumerable<ApiResource> GetApiResources()
         {
             return new List<ApiResource>
-    {
-        new ApiResource("api1", "My API")
-    };
+                    {
+                        new ApiResource("api1", "My API")
+                    };
         }
 
         public static IEnumerable<Client> GetClients()
         {
             return new List<Client>
-    {
-        new Client
-        {
-            ClientId = "client",
-
-            // no interactive user, use the clientid/secret for authentication
-            AllowedGrantTypes = GrantTypes.ClientCredentials,
-
-            // secret for authentication
-            ClientSecrets =
             {
-                new Secret("secret".Sha256())
-            },
+                new Client
+                {
+                    ClientId = "client",
 
-            // scopes that client has access to
-            AllowedScopes = { "api1" }
-        },
-                new Client{
-                    ClientId = "ro.client",
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    ClientSecrets = {
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
                         new Secret("secret".Sha256())
                     },
-                    AllowedScopes = {"api1"}
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
                 },
 
-                new Client{
-                    ClientId = "mvc",
-                    ClientName = "MVC Client",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    RedirectUris = {"https://localhost:5005/signin-oidc"},
-                    PostLogoutRedirectUris = {"https://localhost:5005/signout-callback-oidc"},
-                    AllowedScopes = {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile
-                    }
-                }
-    };
+                new Client
+                {
+                            ClientId = "ro.client",
+                            AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                            ClientSecrets = {
+                                new Secret("secret".Sha256())
+                            },
+                            AllowedScopes = {"api1"}
+                        },
+
+                new Client
+                {
+                            ClientId = "mvc",
+                            ClientName = "MVC Client",
+                            AllowedGrantTypes = GrantTypes.Implicit,
+                            RedirectUris = {"https://localhost:5005/signin-oidc"},
+                            PostLogoutRedirectUris = {"https://localhost:5005/signout-callback-oidc"},
+                            AllowedScopes = {
+                                IdentityServerConstants.StandardScopes.OpenId,
+                                IdentityServerConstants.StandardScopes.Profile
+                            }
+                 }
+            };
         }
 
         public static List<TestUser> GetUsers()
         {
             return new List<TestUser>
-    {
-        new TestUser
-        {
-            SubjectId = "1",
-            Username = "alice",
-            Password = "password"
-        },
-        new TestUser
-        {
-            SubjectId = "2",
-            Username = "bob",
-            Password = "password"
-        }
-    };
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
         }
 
         public static IEnumerable<IdentityResource> GetIdentityResources()
         {
             return new List<IdentityResource>
-    {
-        new IdentityResources.OpenId(),
-        new IdentityResources.Profile(),
-    };
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
         }
     }
 }
